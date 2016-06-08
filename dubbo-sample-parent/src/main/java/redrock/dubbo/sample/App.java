94d98bf45d235a7dbcaed7e4258cdef7528822ce
liu shuang <liushuangqwe1117@163.com>
2016-06-08 23:34:10 +0800

    dubbo parent project

---
+++ dubbo-sample-parent/src/main/java/redrock/dubbo/sample/App.java
@@ -0,0 +1,13 @@
+package redrock.dubbo.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
